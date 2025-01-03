d4d4b6e1559e383092334b625f1bc94d0dc29da2
kouts <koutsaftakis@gmail.com>
2025-01-03 18:13:28 +0200

    fix: fixed types in vue plugin

--- vite-plugins/vue/index.ts
+++ vite-plugins/vue/index.ts
@@ -3,19 +3,18 @@ This plugin adds additional config to the Vue Vite plugin so that it strips data
 Reference: https://github.com/vitejs/vite/issues/636#issuecomment-665545551
 */
 import vue from '@vitejs/plugin-vue'
+import { type ElementNode, type Node } from '@vue/compiler-core'
 
 const isProd = process.env.NODE_ENV === 'production'
 
 const attrsToStrip = ['test-id', 'data-testid', 'data-testclass']
 
-// Typing the "node" param properly would require us to install and import the @vue/compiler-core package
-
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-const stripAttrs = (node: any) => {
+const stripAttrs = (node: Node) => {
   // If the node is an element node
   if (node.type === 1) {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    node.props = node.props.filter((prop: any) => {
+    const elementNode = node as ElementNode
+
+    elementNode.props = elementNode.props.filter((prop) => {
       // If the prop is an attr
       if (prop.type === 6) {
         return !attrsToStrip.includes(prop.name)
